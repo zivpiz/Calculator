ef9a60892f8455cc407b52aee624855eebcc53ed
Ziv Pizanti <zivp@ZivP-MBP13.local>
2018-11-28 10:17:10 +0200

    Fixes in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -17,13 +17,13 @@ module.exports = {
         })],
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
+            // {
+            //     test: /\.js$/,
+            //     exclude: /node_modules/,
+            //     use: {
+            //         loader: 'babel-loader'
+            //     }
+            // },
             {
                 test: /\.css$/,
                 use: [
